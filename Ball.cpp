681569ef43187c3354dd1ccf9ed61fdb711fd1e6
Roger Bassons <rogerbassons@gmail.com>
2015-02-07 17:25:23 +0100

    Re-writed some parts

--- Ball.cpp
+++ Ball.cpp
@@ -1,70 +1,91 @@
 //Copyright 2015 Roger Bassons
 #include "Ball.h"
 #include <cmath>
-#include <ctime>
 #include <random>
 
 Ball::Ball() {
 	b_.x = b_.y = b_.w = b_.h = 0;
-	speed_ = 7;
+	launchSpeed_ = 6;
+	dx_ = 6;
 	dy_ = 0;
-	dx_ = 7;
-	srand(time(NULL)); 
 }
 
 void Ball::setSize(int size) {
 	b_.w = b_.h = size;
 }
 
-void Ball::setPosition(int x, int y) {
-	b_.x = x;
-	b_.y = y;
+void Ball::setLaunchSpeed(int speed) {
+	launchSpeed_ = speed;
 }
 
-bool Ball::move(int width, int height, const Paddle &left, const Paddle &right) {
-	int nextX = b_.x + dx_;
-	int nextY = b_.y + dy_;
+void Ball::launch(int width, int height) {
 
-	if (nextX > width or nextX+b_.w < 0) {
-		nextX = width/2-b_.w/2;
-		nextY = height/2-b_.h;
+	b_.x = width/2 - b_.w/2;
+	b_.y = height/2 - b_.h;
+	
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	
+	std::uniform_int_distribution<int> dist1(0,1);
+	int direction = dist1(mt);
+	if (direction == 0)
+		direction = -1;
 		
-		int min = 0;
-		int max = 65;
-		int angle = rand()%(max-min + 1) + min;                                        
+	std::uniform_int_distribution<int> dist2(0,360);
+	int angle = dist2(mt);
+       
+	dx_ = direction * launchSpeed_ * cos(angle * M_PI/180.0);               
+	dy_ = launchSpeed_ * sin(angle * M_PI/180.0);
+	
+	if (dx_ == 0)
+		dx_ = 5;
+}
 
-		min = 0;
-	        max = 1;
-		int direction = rand()%(max-min + 1) + min;
-		if (direction == 0)
-			direction = -1;
-		
-		dx_ = direction*speed_*cos(angle*M_PI/180.0);               
-		dy_ = speed_*sin(angle*M_PI/180.0);
-	} else if (nextY < 0) {
-		dy_*=-1;
-		nextY = 0;
-	} else if (nextY+b_.h > height) {
-		dy_*=-1;
-		nextY = height-b_.h;
-	}
-
+bool Ball::hasCollidedWithAnyPaddle(int width, int height, const Paddle &left, const Paddle &right) {
 	bool collided = false;
 	if (dx_ < 0 and b_.x < width/2) {
-		if (b_.x >= left.getX() and b_.x <= left.getX()+left.getWidth()) {
-			if (b_.y >= left.getY() and b_.y <= left.getY()+left.getHeight())
+		if (b_.x >= left.getX() and b_.x <= left.getX() + left.getWidth()) {
+			
+			if (b_.y >= left.getY() and b_.y <= left.getY() + left.getHeight())
 				collided = true;
-			else if (b_.y+b_.h >= left.getY() and b_.y+b_.h <= left.getY()+left.getHeight())
+			
+			else if (b_.y + b_.h >= left.getY() and b_.y + b_.h <= left.getY() + left.getHeight())
 				collided = true;
+			
 		}
 	} else if (dx_ > 0 and b_.x > width/2) {
-		if (b_.x+b_.w >= right.getX() and b_.x+b_.w <= right.getX()+right.getWidth()) {
-			if (b_.y >= right.getY() and b_.y <= right.getY()+right.getHeight())
+		if (b_.x + b_.w >= right.getX() and b_.x + b_.w <= right.getX() + right.getWidth()) {
+			
+			if (b_.y >= right.getY() and b_.y <= right.getY() + right.getHeight())
 				collided = true;
-			else if (b_.y+b_.h >= right.getY() and b_.y+b_.h <= right.getY()+right.getHeight())
+			
+			else if (b_.y + b_.h >= right.getY() and b_.y + b_.h <= right.getY() + right.getHeight())
 				collided = true;
+			
 		}
 	}
+	return collided;
+}
+
+bool Ball::move(int width, int height, const Paddle &left, const Paddle &right, int &winner) {
+	winner = -1;
+	
+	int nextX = b_.x + dx_;
+	int nextY = b_.y + dy_;
+	
+	if (nextX > width) {
+		winner = 1;
+	} else if (nextX + b_.w < 0) {
+		winner = 0;
+	} else if (nextY < 0) {
+		dy_ *= -1;
+		nextY = 0;
+	} else if (nextY + b_.h > height) {
+		dy_ *= -1;
+		nextY = height - b_.h;
+	}
+
+	bool collided = hasCollidedWithAnyPaddle(width,height,left,right);
 	if (collided) {
 		dx_ *= -1;
 		if (dy_ == 0)
@@ -77,8 +98,8 @@ bool Ball::move(int width, int height, const Paddle &left, const Paddle &right) 
 }
 
 void Ball::moreSpeed() {
-	speed_++;
 	dx_++;
+	dy_++;
 }
 
 void Ball::draw(SDL_Renderer *ren) const {
--- Ball.h
+++ Ball.h
@@ -6,18 +6,20 @@ class Ball {
  public:
 	Ball();
 	void setSize(int size);
-	void setPosition(int x, int y);
-
-	// move inside rectangle width*height
-	bool move(int width, int height, const Paddle &left, const Paddle &right);
-	void moreSpeed();
+	void setLaunchSpeed(int speed);
 	
+	// inside rectangle width*height
+	void launch(int width, int height);
+	bool move(int width, int height, const Paddle &left, const Paddle &right, int &winner);
+	void moreSpeed();
 	
 	void draw(SDL_Renderer *ren) const;
 
  private:
 	SDL_Rect b_;
-	int speed_;
+	int launchSpeed_;
 	int dx_,dy_;
+
+	bool hasCollidedWithAnyPaddle(int width, int height, const Paddle &left, const Paddle &right);
 };
 	
